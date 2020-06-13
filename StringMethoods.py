41bd6d0fc8ff3fc271936a3032571eb277052fc1
Çağrı ATASEVEN <cataseven3443@gmail.com>
2020-06-13 13:59:44 +0200

    Add files via upload

---
+++ StringMethoods.py
@@ -0,0 +1,29 @@
+#  Methood of String
+
+#strip
+a= "abc xyz klm"
+a.strip("a")
+
+#replace
+
+a.replace("a","1")
+ 
+b="abc xyz"
+
+b.title()
+
+#Substring
+
+word="python is a OOP language"
+word[0]
+word[1:3]
+
+len(word)
+word[23]
+ 
+
+
+
+
+
+
