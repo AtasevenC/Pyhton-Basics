57724200b63926f72a11c720993aa7d445b40826
Çağrı ATASEVEN <cataseven3443@gmail.com>
2020-06-13 13:59:17 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,39 @@
+# Methots-Functions
+
+def sum(a,b):
+    print(a+b)
+    
+sum(3,4)
+
+def sq(x):
+    print(x**2)
+    
+sq(9)
+
+# sep function
+print("a","b",sep="-")
+
+def times(a,b):
+    '''
+   "This is times function"
+    '''
+    print(a*b)
+   
+   
+    
+times(4,5)
+
+#When we write any return function?
+# if we want to use the output of function 
+#then we have to use return. 
+
+def calculate(a,b,c):
+    return (a+b)/c
+
+calculate(3,4,5)
+
+calculate(1,2,3)*4
+
+for i in range(1,10,2):
+    print(i)
+    
